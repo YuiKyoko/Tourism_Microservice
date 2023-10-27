fb4f47ff6b6789d2142095d9e3bad60598ad0024
Kimberly <kimberlyesquivelgala@gmail.com>
2023-10-27 00:30:21 -0500

    first commit

---
+++ Proyecto_DSW/ConsultaLugaresTuristicos/LugarTuristico.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto_DSW.ConsultaLugaresTuristicos
+{
+    public class LugarTuristico
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public string Categoria { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+    }
+}
---
+++ Proyecto_DSW/ConsultaLugaresTuristicos/LugaresTuristicosController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto_DSW.ConsultaLugaresTuristicos
+{
+    public class LugaresTuristicosController : Controller
+    {
+         [HttpGet]
+        public IEnumerable<LugarTuristico> Get()
+        {
+            // Realiza una consulta a la base de datos para obtener todos los lugares turísticos.
+
+            return _context.LugaresTuristicos.ToList();
+        } 
+        
+        
+        
+        // GET: LugaresTuristicosController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LugaresTuristicosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LugaresTuristicosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LugaresTuristicosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LugaresTuristicosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LugaresTuristicosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LugaresTuristicosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LugaresTuristicosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+      
+
+    }
+}
